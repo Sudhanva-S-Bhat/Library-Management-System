35b32a23270758d01a676bd996d724e63981476a
Sudhanva-S-Bhat <sudhanvasbhat13@gmail.com>
2026-06-14 18:22:53 +0530

    Deploy Bug Fix

--- App.js
+++ App.js
@@ -364,4 +364,7 @@ app.get('/logout',(request,response)=>{
     console.log('running in port 5000')
 })*/
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
